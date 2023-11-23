2f02c58c118dd12d16ab9e60681ba4828a5d67db
aekanut <aekanut.rungseevuttana@gmail.com>
2023-11-24 00:44:40 +0700

    feat: init typegoose + nestjs/mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './configuration';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const { uri, dbName } = configService.get<{
+          uri: string;
+          dbName: string;
+        }>('mongodb');
+        return {
+          uri,
+          dbName,
+          autoCreate: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
   ],
   controllers: [],
   providers: [],
--- src/configuration.ts
+++ src/configuration.ts
@@ -2,8 +2,8 @@ export default () => ({
   server: {
     port: parseInt(process.env.SERVER_PORT, 10) || 3000,
   },
-  mongo: {
-    host: process.env.MONGO_HOST || 'mongodb://127.0.0.1:27017',
-    databaseName: process.env.MONGO_DATABASE_NAME || 'master-country',
+  mongodb: {
+    uri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017',
+    dbName: process.env.MONGO_DB_NAME || 'master-country',
   },
 });
